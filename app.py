eca8ba27949e73364f9a6982f73876ce8f77ec67
alfinofathinzaky-sudo <alfinofathinzaky@gmail.com>
2026-02-04 18:26:29 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
+HEADERS = {"Authorization": "Bearer ISI_TOKEN_HUGGINGFACE_KAMU"}
+
+def analisis_sentimen(teks):
+    response = requests.post(
+        API_URL,
+        headers=HEADERS,
+        json={"inputs": teks}
+    )
+    return response.json()
+
+st.set_page_config(page_title="UAS Rekayasa Fitur", layout="centered")
+st.title("Analisis Sentimen Teks")
+st.write("UAS Rekayasa Fitur – Analisis Sentimen menggunakan API")
+
+teks = st.text_area("Masukkan teks yang ingin dianalisis:")
+
+if st.button("Analisis Sentimen"):
+    if teks.strip() == "":
+        st.warning("Teks tidak boleh kosong!")
+    else:
+        hasil = analisis_sentimen(teks)
+        label = hasil[0][0]["label"]
+        skor = hasil[0][0]["score"]
+
+        st.success(f"Hasil Sentimen: {label}")
+        st.info(f"Tingkat Kepercayaan: {skor:.2f}")
